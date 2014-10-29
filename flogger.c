a774e944a31f9a3c6bc347c2112969ad3abd9757
CurtisFielding <fieldic@hotmail.com>
2014-10-28 19:14:35 -0700

    fixed Makefile, added set_pg_rw

--- flogger.c
+++ flogger.c
@@ -5,8 +5,11 @@
 #include <linux/file.h>
 #include <linux/fs.h>
 #include <linux/fcntl.h>
+/* #include <asm/semaphore.h> */
+#include <asm/cacheflush.h>
 #include <asm/uaccess.h>
 #include <asm/syscall.h>
+
 
 
 extern const sys_call_ptr_t sys_call_table[];
@@ -40,6 +43,20 @@ asmlinkage size_t flog_read(int filedes, void *buf, size_t bytes)
   printk("it\'s flogging time!\n");
   floglog(flogf, buf);
   return og_sys_read(filedes, buf, bytes);
+}
+
+int set_pg_rw(long unsigned int addr)
+{
+  struct page *pg;
+  pgprot_t prot;
+  pg = virt_to_page(addr);
+  prot.pgprot = VM_READ | VM_WRITE;
+  return change_page_attr(pg, 1, prot);
+}
+
+int set_pg_ro(long unsigned int addr)
+{
+
 }
 
 int init_module()
